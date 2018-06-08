fed6214b7876feee836ff5c13011b7dcd6b5f820
Weijie He <heweijie@gih-d-12076.local>
2018-06-08 14:15:31 +0800

    Initial Commit

---
+++ VJsonPaser/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VJsonPaser
+//
+//  Created by Weijie He on 2018/6/8.
+//  Copyright © 2018年 baostorm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
